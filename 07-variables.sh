7a49b271b82d0ae2b5ad29f30ba470dbfa591845
sundhar <aitechwithsundhar@gmail.com>
2026-02-05 23:25:25 +0700

    actions

--- 07-variables.sh
+++ 07-variables.sh
@@ -6,5 +6,6 @@ START_TIME=$(date +%s)
 echo "scripit executed at: $START_TIME"
 sleep 10
 END_TIME=$(date +%s)
-TOTAL_time=$(($END_TIME-$START_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
 echo "script executed in $TOTAL_TIME"
