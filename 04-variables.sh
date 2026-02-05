767aa7ec6dbb563ffe7223d62eecba96f0eda4e5
sundhar <aitechwithsundhar@gmail.com>
2026-02-05 22:44:20 +0700

    actions

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=$1 # no space between value and =
+PERSON2=$2 
+
+
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: hi $PERSON1, i am fine thanks for asking. how are you doing?"
+echo "$PERSON1:: am doing great. what are you learning $PERSON2?"
+echo "$PERSON2:: am leraning DevOps, what about you $PERSON1?"
+echo "$PERSON1:: am learning linux commands and shell scripting"
