f8f99b294663ea6512a9c2e0e5f25baeb973762e
sundhar <aitechwithsundhar@gmail.com>
2026-02-05 21:52:11 +0700

    add 02-conversation shell script

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,5 +2,5 @@
 echo "suresh:: hello ramesh, how are you?"
 echo "ramesh:: hi suresh, i am fine thanks for asking. how are you doing?"
 echo "suresh:: am doing great. what are you learning ramesh?"
-echao "ramesh:: am leraning DevOps, what about you suresh?"
+echo "ramesh:: am leraning DevOps, what about you suresh?"
 echo "suresh:: am learning linux commands and shell scripting"
