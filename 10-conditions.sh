8a8400b0b7b5d0422140c6f991338669a3ca560a
sundhar <aitechwithsundhar@gmail.com>
2026-02-07 11:43:12 +0700

    actions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -11,8 +11,9 @@ number=$1
 if [ $number -gt 20 ]; then 
     echo "given number: $number is greater then 20"
 
-elif [ $numeber -eq 20 ] then 
-    echo "given number: $ is equal to 20"
+elif [ $numeber -eq 20 ]; then 
+    echo "given number: $number is equal to 20"
+
 else 
     echo "given nuber: $number is less then 20"
 
