a35412c8aacf838bbd2a6be825a111713e1f9889
sundhar <aitechwithsundhar@gmail.com>
2026-02-07 11:27:15 +0700

    actions

---
+++ 10-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+number=25 
+
+# -gt -> greater than
+# -lt -> less then 
+# -eq -> equal 
+# -ne -> not equal
+
+if [$number -gt 20]; then 
+    echo "given number: $number is greater then 20"
+fi     
