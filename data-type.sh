4c5422802b343acb5f1d5e809ecfdd6798677243
sundhar <aitechwithsundhar@gmail.com>
2026-02-07 11:02:45 +0700

    actions

--- data-type.sh
+++ data-type.sh
@@ -9,4 +9,7 @@ SUM=$(($NUM1+$NUM2))
 echo "sum is: $SUM"
 
 fruits=("Apple" "banana" "mango")
-echo "fruits are :${fruits[@]}"
+echo "fruits are : ${fruits[@]}"
+echo "first fruit: ${fruits[0]}"
+echo "second fruit: ${fruits[1]}"
+echo "third fruit: ${fruits[2]}"
