20bf83bb0d51bf5038ec73dd944e46e0f77fd7ae
sundhar <aitechwithsundhar@gmail.com>
2026-02-09 21:31:14 +0700

    actions

--- 16-colors.sh
+++ 16-colors.sh
@@ -15,12 +15,12 @@ LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 R="\e[31"
 G="\e[32"
 Y="\e[33"
-N="\e[34"
+N="\e[0m"
 
 
 # Check root access
 if [ $USERID -ne 0 ]; then
-    echo -e "$G Please run this script with root user access $N" | tee -a $LOGS_FILE
+    echo -e "${R} Please run this script with root user access ${N}" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -30,10 +30,10 @@ mkdir -p "$LOGS_FOLDER"
 # Validation function
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ....$R FAILURE" $N | tee -a $LOGS_FILE
+        echo -e "$2 ....${R}FAILURE${N}" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$2 ....$G SUCCESS" $Y | tee -a $LOGS_FILE
+        echo -e "$2 ....${G}SUCCESS${N}" | tee -a $LOGS_FILE
     fi
 }
 
@@ -41,11 +41,11 @@ for package in "$@" #sudo sh 15-loops.sh <package name>
 do 
     dnf list instyalled $package &>>$LOGS_FILE
     if [ $? -ne 0 ]; then
-        echo "$package $Y not installed, install now $N"
+        echo "$package ${Y} not installed, install now ${N}"
         dnf install $package -y &>>$LOGS_FILE
         VALIDATE $? "$package installation"
     else 
-        echo -e "$package $G already installed, skipping $N"
+        echo -e "$package ${G} already installed, skipping ${N}"
         
     fi
 done 
